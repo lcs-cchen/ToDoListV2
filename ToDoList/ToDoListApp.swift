c9631bede1459957c2836adf5efcdeeb57d1b425
cyrus chen <pschen@lcs.on.ca>
2023-04-03 14:07:44 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Cyrus Chen on 3/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
